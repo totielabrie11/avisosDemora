a6520318aae98da2de0ba670fac3fd4267e5a120
totielabrie11 <martinezcarlosmanuel.1@gmail.com>
2024-08-16 09:59:22 -0300

    mejora en el manejo de modalText

--- frontend/src/component/ModalText.jsx
+++ frontend/src/component/ModalText.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Modal, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
-import { BACKEND_URL } from '../config'; // Importa la URL del backend desde config
+import { BACKEND_URL } from '../config';
 
 const ModalText = ({ show, onHide, pedido, estado, onSubmit, token, usuario }) => {
   const [prioridad, setPrioridad] = useState(estado === 'no vencido' ? 'Normal' : 'Regular');
@@ -12,13 +12,22 @@ const ModalText = ({ show, onHide, pedido, estado, onSubmit, token, usuario }) =
   useEffect(() => {
     if (show) {
       setPrioridad(estado === 'no vencido' ? 'Normal' : 'Regular');
-      setMensaje(estado === 'no vencido' ? `El operador ${usuario}, consulta si Vamos a poder cumplir con este pedido?` : `El operador ${usuario}, indica que tenemos una demora de entrega en este pedido.`);
+      setMensaje(
+        estado === 'no vencido' 
+          ? `El operador ${usuario}, consulta si Vamos a poder cumplir con este pedido?` 
+          : `El operador ${usuario}, indica que tenemos una demora de entrega en este pedido.`
+      );
       setError('');
 
       if (estado === 'no vencido') {
         const allItems = {};
-        pedido.Items.forEach((item) => {
-          allItems[item.Codigo] = { ...item, cantidad: item.Cantidad };
+        pedido.Items.forEach((item, index) => {
+          allItems[index] = {
+            id: index,
+            ...item,
+            cantidad: item.Cantidad,
+            selected: true
+          };
         });
         setSelectedItems(allItems);
       } else {
@@ -35,39 +44,61 @@ const ModalText = ({ show, onHide, pedido, estado, onSubmit, token, usuario }) =
     setMensaje(e.target.value);
   };
 
-  const handleItemChange = (item, cantidad) => {
+  const handleItemChange = (id, cantidad) => {
     setSelectedItems((prevSelectedItems) => ({
       ...prevSelectedItems,
-      [item.Codigo]: { ...item, cantidad },
+      [id]: {
+        ...prevSelectedItems[id],
+        cantidad,
+        selected: cantidad > 0
+      }
     }));
   };
 
   const handleSelectAll = () => {
     const allItems = {};
-    pedido.Items.forEach((item) => {
-      allItems[item.Codigo] = { ...item, cantidad: item.Cantidad };
+    pedido.Items.forEach((item, index) => {
+      allItems[index] = {
+        id: index,
+        ...item,
+        cantidad: item.Cantidad,
+        selected: true
+      };
+    });
+    setSelectedItems(allItems);
+  };
+
+  const handleDeselectAll = () => {
+    const allItems = {};
+    pedido.Items.forEach((item, index) => {
+      allItems[index] = {
+        id: index,
+        ...item,
+        cantidad: 0,
+        selected: false
+      };
     });
     setSelectedItems(allItems);
   };
 
   const handleSubmit = async () => {
     const material = Object.values(selectedItems)
-      .filter(item => item.cantidad > 0)
-      .map(item => ({
+      .filter((item) => item.selected && item.cantidad > 0)
+      .map((item) => ({
         codigo: item.Codigo,
         cantidad: item.cantidad,
         descripcion: item.Descripcion,
-        fechaVencimiento: item.Fecha_vencida
+        fechaVencimiento: item.Fecha_vencida,
       }));
 
     const reclamo = {
       pedido: pedido.Pedido,
       cliente: pedido.Cliente,
-      oc: pedido.oc, // Incluye el valor de OC en el reclamo
+      oc: pedido.oc,
       estado,
       prioridad,
       mensaje,
-      material
+      material,
     };
 
     try {
@@ -82,8 +113,15 @@ const ModalText = ({ show, onHide, pedido, estado, onSubmit, token, usuario }) =
       onSubmit(reclamo);
       onHide();
     } catch (error) {
-      console.error('Error enviando el reclamo:', error.response ? error.response.data : error);
-      setError(`Error enviando el reclamo: ${error.response ? error.response.data.error : 'Desconocido'}`);
+      console.error(
+        'Error enviando el reclamo:',
+        error.response ? error.response.data : error
+      );
+      setError(
+        `Error enviando el reclamo: ${
+          error.response ? error.response.data.error : 'Desconocido'
+        }`
+      );
     }
   };
 
@@ -103,7 +141,12 @@ const ModalText = ({ show, onHide, pedido, estado, onSubmit, token, usuario }) =
           </Form.Group>
           <Form.Group>
             <Form.Label>Prioridad</Form.Label>
-            <Form.Control as="select" value={prioridad} onChange={handlePrioridadChange} disabled={estado === 'no vencido'}>
+            <Form.Control
+              as="select"
+              value={prioridad}
+              onChange={handlePrioridadChange}
+              disabled={estado === 'no vencido'}
+            >
               {estado === 'no vencido' ? (
                 <>
                   <option>Normal</option>
@@ -128,24 +171,34 @@ const ModalText = ({ show, onHide, pedido, estado, onSubmit, token, usuario }) =
           </Form.Group>
           <Form.Group>
             <Form.Label>Items del Pedido</Form.Label>
-            <Button variant="link" onClick={handleSelectAll}>
-              Seleccionar Todo
-            </Button>
-            {pedido.Items.map((item) => (
-              <div key={item.Codigo} className="mb-2">
+            <div className="d-flex justify-content-between mb-2">
+              <Button variant="link" onClick={handleSelectAll}>
+                Seleccionar Todo
+              </Button>
+              <Button variant="link" onClick={handleDeselectAll}>
+                Deseleccionar Todo
+              </Button>
+            </div>
+            {pedido.Items.map((item, index) => (
+              <div key={index} className="mb-2">
                 <Form.Check
                   type="checkbox"
                   label={`${item.Descripcion} - Cantidad: ${item.Cantidad}`}
-                  onChange={(e) => handleItemChange(item, e.target.checked ? item.Cantidad : 0)}
-                  checked={!!selectedItems[item.Codigo]}
+                  onChange={(e) =>
+                    handleItemChange(index, e.target.checked ? item.Cantidad : 0)
+                  }
+                  checked={selectedItems[index]?.selected || false}
                 />
-                {selectedItems[item.Codigo]?.cantidad > 0 && (
+                {selectedItems[index]?.selected && (
                   <Form.Control
                     type="number"
-                    value={selectedItems[item.Codigo].cantidad}
+                    value={selectedItems[index].cantidad}
                     min="1"
                     max={item.Cantidad}
-                    onChange={(e) => handleItemChange(item, Number(e.target.value))}
+                    onChange={(e) => {
+                      const cantidad = Number(e.target.value);
+                      handleItemChange(index, cantidad);
+                    }}
                     className="mt-1"
                   />
                 )}
